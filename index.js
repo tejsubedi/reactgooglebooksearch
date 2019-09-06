c00a9abd9332a4049ef81fa668387421402d37de
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-05 21:52:17 -0400

    remove dotenv npm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-require('dotenv').config();
+// require('dotenv').config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +21,8 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 app.use(logger("dev"));
+
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactbook" , {useNewUrlParser: true });
 
